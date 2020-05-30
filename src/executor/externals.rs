8dcf93b890b328272da383f396ae8ff309124bf3
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-05-30 15:47:14 +0200

    Define all externals

--- src/executor/externals.rs
+++ src/executor/externals.rs
@@ -206,6 +206,291 @@ impl<'a, T> StateWaitExternalResolve<'a, T> {
 
 fn get_function(name: &str) -> RegisteredFunction {
     match name {
+        "ext_storage_set_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_get_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_read_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_clear_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_exists_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_clear_prefix_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_root_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_changes_root_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_next_key_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_set_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_get_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_read_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_clear_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_storage_kill_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_exists_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_clear_prefix_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_root_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_storage_child_next_key_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_ed25519_public_keys_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_ed25519_generate_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_ed25519_sign_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_ed25519_verify_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_sr25519_public_keys_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_sr25519_generate_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_sr25519_sign_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_sr25519_verify_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_secp256k1_ecdsa_recover_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_crypto_secp256k1_ecdsa_recover_compressed_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_keccak_256_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_sha2_256_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_blake2_128_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_blake2_256_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_twox_64_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_twox_128_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_hashing_twox_256_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_is_validator_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_submit_transaction_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_network_state_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_timestamp_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_sleep_until_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_random_seed_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_local_storage_set_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_local_storage_compare_and_set_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_local_storage_get_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_request_start_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_request_add_header_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_request_write_body_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_response_wait_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_response_headers_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_offchain_http_response_read_body_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_trie_blake2_256_root_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_trie_blake2_256_ordered_root_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_misc_chain_id_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_misc_print_num_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_misc_print_utf8_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_misc_print_hex_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
+        "ext_misc_runtime_version_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
+            }
+        }
         "ext_allocator_malloc_version_1" => {
             RegisteredFunction::Immediate {
                 implementation: Box::new(move |params, vm, alloc| {
@@ -230,6 +515,11 @@ fn get_function(name: &str) -> RegisteredFunction {
                     alloc.deallocate(&mut MemAccess(vm), param).unwrap(); // TODO: don't unwrap
                     None
                 }),
+            }
+        }
+        "ext_logging_log_version_1" => {
+            RegisteredFunction::Immediate {
+                implementation: Box::new(|_, _, _| unimplemented!()),
             }
         }
         _ => RegisteredFunction::Unresolved {
