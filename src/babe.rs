7cac4dfc52cf3dd788a16f160301be0d48a88315
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-07-12 20:22:56 +0200

    Work

--- src/babe.rs
+++ src/babe.rs
@@ -70,9 +70,22 @@ pub mod chain_config;
 
 pub use chain_config::BabeGenesisConfiguration;
 
-/// Failed to verify a block.
+/// Failure to verify a block.
 #[derive(Debug, Clone, derive_more::Display)]
-pub enum VerifyError {}
+pub enum VerifyError {
+    /// Header passed is of the wrong format.
+    InvalidHeader,
+    /// The seal (containing the signature of the authority) is missing from the header.
+    MissingSeal,
+    /// No pre-runtime digest in the block header.
+    MissingPreRuntimeDigest,
+    /// There are multiple pre-runtime digests in the block header.
+    MultiplePreRuntimeDigests,
+    /// Failed to decode pre-runtime digest.
+    PreRuntimeDigestDecodeError(parity_scale_codec::Error),
+    /// Failed to decode a consensus digest.
+    ConsensusDigestDecodeError(parity_scale_codec::Error),
+}
 
 /// Configuration for [`verify_header`].
 pub struct VerifyConfig<'a> {
@@ -88,6 +101,76 @@ pub struct VerifyConfig<'a> {
 
 /// Verifies whether a block header provides a correct proof of the legitimacy of the authorship.
 pub fn verify_header(config: VerifyConfig) -> Result<(), VerifyError> {
+    let header = crate::block::Header::decode_all(config.scale_encoded_header)
+        .map_err(|_| VerifyError::InvalidHeader)?;
+
+    // TODO: idea: move the information extraction to a separate module, to split the extraction
+    // from verification; this way, users can simply examine a header
+
+    // Part of the rules is that the last digest log of the header must always be the seal,
+    // containing a signature of the rest of the header and made by the author of the block.
+    let seal_signature: &Vec<u8> = header
+        .digest
+        .logs
+        .last()
+        .and_then(|l| match l {
+            crate::block::DigestItem::Seal(engine, signature) if engine == b"BABE" => {
+                Some(signature)
+            }
+            _ => None,
+        })
+        .ok_or(VerifyError::MissingSeal)?;
+
+    // Additionally, one of the digest logs of the header must be a BABE pre-runtime digest whose
+    // content contains the slot claim made by the author.
+    let pre_runtime: definitions::PreDigest = {
+        let mut pre_runtime_digests = header.digest.logs.iter().filter_map(|l| match l {
+            crate::block::DigestItem::PreRuntime(engine, data) if engine == b"BABE" => Some(data),
+            _ => None,
+        });
+        let pre_runtime = pre_runtime_digests
+            .next()
+            .ok_or(VerifyError::MissingPreRuntimeDigest)?;
+        if pre_runtime_digests.next().is_some() {
+            return Err(VerifyError::MultiplePreRuntimeDigests);
+        }
+        definitions::PreDigest::decode_all(&pre_runtime)
+            .map_err(VerifyError::PreRuntimeDigestDecodeError)?
+    };
+
+    // Finally, the header can contain consensus digest logs, indicating an epoch transition or
+    // a configuration change.
+    let consensus_logs: Vec<definitions::ConsensusLog> = {
+        let list = header.digest.logs.iter().filter_map(|l| match l {
+            crate::block::DigestItem::Consensus(engine, data) if engine == b"BABE" => Some(data),
+            _ => None,
+        });
+
+        let mut consensus_logs = Vec::with_capacity(header.digest.logs.len());
+        for digest in list {
+            let decoded = definitions::ConsensusLog::decode_all(&digest)
+                .map_err(VerifyError::ConsensusDigestDecodeError)?;
+            consensus_logs.push(decoded)
+        }
+        consensus_logs
+    };
+
+    // The signature of the block header applies to the header from where the signature isn't
+    // present.
+    let pre_seal_hash = {
+        let mut unsealed_header = header.clone();
+        let _popped = unsealed_header.digest.logs.pop();
+        debug_assert!(matches!(
+            _popped,
+            Some(crate::block::DigestItem::Seal(_, _))
+        ));
+        unsealed_header.block_hash()
+    };
+
+    if !consensus_logs.is_empty() {
+        println!("logs: {:?}", consensus_logs);
+    }
+
     // TODO:
     Ok(())
 }
--- src/babe/definitions.rs
+++ src/babe/definitions.rs
@@ -2,6 +2,7 @@
 
 // TODO: decide what to do with this module; it's obviously not great
 
+use core::fmt;
 use parity_scale_codec::{Decode, Encode};
 
 /// BABE epoch information
@@ -77,4 +78,116 @@ pub enum AllowedSlots {
     PrimaryAndSecondaryPlainSlots,
     /// Allow primary and secondary VRF slots.
     PrimaryAndSecondaryVRFSlots,
+}
+
+/// An consensus log item for BABE.
+#[derive(Debug, Decode, Encode, Clone, PartialEq, Eq)]
+pub enum ConsensusLog {
+    /// The epoch has changed. This provides information about the _next_
+    /// epoch - information about the _current_ epoch (i.e. the one we've just
+    /// entered) should already be available earlier in the chain.
+    #[codec(index = "1")]
+    NextEpochData(NextEpochDescriptor),
+    /// Disable the authority with given index.
+    #[codec(index = "2")]
+    OnDisabled(u32),
+    /// The epoch has changed, and the epoch after the current one will
+    /// enact different epoch configurations.
+    #[codec(index = "3")]
+    NextConfigData(NextConfigDescriptor),
+}
+
+/// Information about the next epoch. This is broadcast in the first block
+/// of the epoch.
+#[derive(Decode, Encode, PartialEq, Eq, Clone, Debug)]
+pub struct NextEpochDescriptor {
+    /// The authorities.
+    pub authorities: Vec<([u8; 32], u64)>,
+
+    /// The value of randomness to use for the slot-assignment.
+    pub randomness: [u8; 32],
+}
+
+/// Information about the next epoch config, if changed. This is broadcast in the first
+/// block of the epoch, and applies using the same rules as `NextEpochDescriptor`.
+#[derive(Decode, Encode, PartialEq, Eq, Clone, Debug)]
+pub enum NextConfigDescriptor {
+    /// Version 1.
+    #[codec(index = "1")]
+    V1 {
+        /// Value of `c` in `BabeEpochConfiguration`.
+        c: (u64, u64),
+        /// Value of `allowed_slots` in `BabeEpochConfiguration`.
+        allowed_slots: AllowedSlots,
+    },
+}
+
+/// A BABE pre-runtime digest. This contains all data required to validate a
+/// block and for the BABE runtime module. Slots can be assigned to a primary
+/// (VRF based) and to a secondary (slot number based).
+#[derive(Clone, Debug, Encode, Decode)]
+pub enum PreDigest {
+    /// A primary VRF-based slot assignment.
+    #[codec(index = "1")]
+    Primary(PrimaryPreDigest),
+    /// A secondary deterministic slot assignment.
+    #[codec(index = "2")]
+    SecondaryPlain(SecondaryPlainPreDigest),
+    /// A secondary deterministic slot assignment with VRF outputs.
+    #[codec(index = "3")]
+    SecondaryVRF(SecondaryVRFPreDigest),
+}
+
+/// Raw BABE primary slot assignment pre-digest.
+#[derive(Clone, Encode, Decode)]
+pub struct PrimaryPreDigest {
+    /// Authority index
+    pub authority_index: u32,
+    /// Slot number
+    pub slot_number: u64,
+    /// VRF output
+    pub vrf_output: [u8; 32],
+    /// VRF proof
+    pub vrf_proof: [u8; 64],
+}
+
+// This custom Debug implementation exists because `[u8; 64]` doesn't implement `Debug`
+impl fmt::Debug for PrimaryPreDigest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PrimaryPreDigest").finish()
+    }
+}
+
+/// BABE secondary slot assignment pre-digest.
+#[derive(Clone, Debug, Encode, Decode)]
+pub struct SecondaryPlainPreDigest {
+    /// Authority index
+    ///
+    /// This is not strictly-speaking necessary, since the secondary slots
+    /// are assigned based on slot number and epoch randomness. But including
+    /// it makes things easier for higher-level users of the chain data to
+    /// be aware of the author of a secondary-slot block.
+    pub authority_index: u32,
+    /// Slot number
+    pub slot_number: u64,
+}
+
+/// BABE secondary deterministic slot assignment with VRF outputs.
+#[derive(Clone, Encode, Decode)]
+pub struct SecondaryVRFPreDigest {
+    /// Authority index
+    pub authority_index: u32,
+    /// Slot number
+    pub slot_number: u64,
+    /// VRF output
+    pub vrf_output: [u8; 32],
+    /// VRF proof
+    pub vrf_proof: [u8; 64],
+}
+
+// This custom Debug implementation exists because `[u8; 64]` doesn't implement `Debug`
+impl fmt::Debug for SecondaryVRFPreDigest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("SecondaryVRFPreDigest").finish()
+    }
 }
