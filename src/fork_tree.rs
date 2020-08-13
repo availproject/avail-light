fe9d249903fa58def4fef135a7f2613516b96166
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-08-13 22:07:15 +0200

    Implement ForkTree::prune_ancestors

--- src/fork_tree.rs
+++ src/fork_tree.rs
@@ -55,10 +55,64 @@ impl<T> ForkTree<T> {
     ///
     /// - The node passed as parameter.
     /// - The ancestors of the node passed as parameter.
-    /// - Any node without any parent.
+    /// - Any node not a descendant of the node passed as parameter.
+    ///
+    /// # Panic
+    ///
+    /// Panics if the [`NodeIndex`] is invalid.
     ///
     pub fn prune_ancestors(&mut self, node_index: NodeIndex) {
-        todo!()
+        // TODO: this function is completely untested
+
+        // The implementation consists in ultimately replacing the content of `self.first_root`
+        // with the content of `self.nodes[node_index].first_child` and updating everything else
+        // accordingly. Save the value here for later.
+        let new_first_root = self.nodes[node_index.0].first_child;
+
+        // Traverse all the nodes, starting from the root, and removing them one by one.
+        let mut iter = self.first_root.unwrap();
+        let mut traversing_up = false;
+        loop {
+            let mut iter_node = &mut self.nodes[iter];
+
+            // If current node is a direct child of `node_index`, then don't remove it.
+            // Instead, just update its parent to be `None` and continue iterating.
+            if iter_node.parent == Some(node_index.0) {
+                debug_assert!(!traversing_up);
+                iter_node.parent = None;
+                iter = if let Some(next_sibling) = iter_node.next_sibling {
+                    next_sibling
+                } else {
+                    traversing_up = true;
+                    node_index.0
+                };
+                continue;
+            }
+
+            // If `traversing_up` is false`, try to go down the hierarchy as deeply as possible.
+            if !traversing_up {
+                if let Some(first_child) = self.nodes[iter].first_child {
+                    iter = first_child;
+                    continue;
+                }
+            }
+
+            // Remove node, then jump either to its next sibling, or, if it was the last sibling,
+            // back to its parent.
+            let iter_node = self.nodes.remove(iter);
+            iter = if let Some(next_sibling) = iter_node.next_sibling {
+                traversing_up = false;
+                next_sibling
+            } else if let Some(parent) = iter_node.parent {
+                traversing_up = true;
+                parent
+            } else {
+                break;
+            };
+        }
+
+        debug_assert!(!self.nodes.contains(node_index.0));
+        self.first_root = new_first_root;
     }
 
     /// Returns all the nodes, starting from the the given node, to the root. Each element
