690b33a41fe19bd5a0c999718d5987da89e1148b
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-09-04 15:47:35 +0200

    Don't use nightly in browser node

--- browser-node/build.sh
+++ browser-node/build.sh
@@ -2,6 +2,6 @@
 
 # Note: this script shows how to build the browser node, but using this script is in no way mandatory
 
-cargo +nightly build --release --target wasm32-unknown-unknown --no-default-features --features wasm-bindings
+cargo build --release --target wasm32-unknown-unknown --no-default-features --features wasm-bindings
 wasm-bindgen ../target/wasm32-unknown-unknown/release/substrate_lite.wasm --out-dir pkg --target web
 python -m http.server 8000
