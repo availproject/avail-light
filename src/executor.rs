31bb80d865f05829c6d610b861399607fdea125d
Ashley <ashley.ruglys@gmail.com>
2021-01-27 19:27:44 +0100

    Add a read only runtime host (#410)
    * Add a read only runtime host
    
    * cargo fmt
    
    * Apply suggestions
    
    * cargo fmt
    
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>

--- src/executor.rs
+++ src/executor.rs
@@ -33,6 +33,7 @@ use parity_scale_codec::DecodeAll as _;
 
 mod allocator; // TODO: make public after refactoring
 pub mod host;
+pub mod read_only_runtime_host;
 pub mod runtime_host;
 pub mod vm;
 
