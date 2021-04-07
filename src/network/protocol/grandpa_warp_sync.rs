2cde77ba4d2ec40286b2fcb1e72932cae551490b
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-04-07 12:20:05 +0200

    Document the GrandPa warp sync protocol (#729)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/network/protocol/grandpa_warp_sync.rs
+++ src/network/protocol/grandpa_warp_sync.rs
@@ -15,29 +15,67 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//! The GrandPa warp sync protocol is a request-response protocol.
+//!
+//! The request's body consists in a block hash.
+//!
+//! The response's body consists in a sequence of so-called *fragments*. Each fragment consists in
+//! a block header and a GrandPa justification corresponding to this header. The justification
+//! must be verified.
+//!
+//! The fragments only contain blocks higher than the hash of the block passed in the request.
+//!
+//! By doing a GrandPa warp sync request, a node is capable of quickly obtaining a proof that a
+//! certain recent block has been finalized by authorities.
+//!
+//! A proof has to be minimum. All the headers in all fragments, except for the last, have to
+//! contain a change in the set of GrandPa authorities.
+//!
+//! The responding node has the possibility to cut proofs that are above a certain threshold. When
+//! it does so, [`GrandpaWarpSyncResponse::is_finished`] should be set to `false`, so that the
+//! requester can start additional warp sync requests afterwards.
+
+use crate::{finality, header};
+
 use alloc::vec::Vec;
 
+// TODO: all the constraints explained here should be checked when decoding the message
+
+/// Response to a GrandPa warp sync request.
 #[derive(Debug)]
 pub struct GrandpaWarpSyncResponse {
+    /// List of fragments that consist in the proof.
+    ///
+    /// The fragments must be ordered by ascending block height.
     pub fragments: Vec<GrandpaWarpSyncResponseFragment>,
+
+    /// If `true`, the last fragment corresponds to the highest finalized block known to the
+    /// responder. If `false`, the requested is encouraged to start a follow-up GrandPa warp sync
+    /// request starting at the last block in the fragments.
     pub is_finished: bool,
 }
 
+/// Response to a GrandPa warp sync request.
 #[derive(Debug)]
 pub struct GrandpaWarpSyncResponseFragment {
-    pub header: crate::header::Header,
-    pub justification: crate::finality::justification::decode::Justification,
+    /// Header of a block in the chain.
+    ///
+    /// Must always contain a change in the list of authorities, except for the last fragment
+    /// if [`GrandpaWarpSyncResponse::is_finished`] is `true`.
+    pub header: header::Header,
+
+    /// Justification that proofs the finality of [`GrandpaWarpSyncResponseFragment::header`].
+    pub justification: finality::justification::decode::Justification,
 }
 
-/// Error returned by [`decode_grandpa_warp_sync_response`].
+/// Error potentially returned by [`decode_grandpa_warp_sync_response`].
 #[derive(Debug, derive_more::Display)]
-pub enum DecodeGrandpaWarpSyncResponseError {
-    BadResponse,
-}
+pub struct DecodeGrandpaWarpSyncResponseError;
 
+/// Decodes a SCALE-encoded GrandPa warp sync response.
 // TODO: make this a zero-cost API
 pub fn decode_grandpa_warp_sync_response(
-    bytes: &[u8],
+    encoded: &[u8],
 ) -> Result<GrandpaWarpSyncResponse, DecodeGrandpaWarpSyncResponseError> {
     nom::combinator::all_consuming(nom::combinator::map(
         nom::sequence::tuple((decode_fragments, nom::number::complete::le_u8)),
@@ -45,9 +83,9 @@ pub fn decode_grandpa_warp_sync_response(
             fragments,
             is_finished: is_finished != 0,
         },
-    ))(bytes)
+    ))(encoded)
     .map(|(_, parse_result)| parse_result)
-    .map_err(|_| DecodeGrandpaWarpSyncResponseError::BadResponse)
+    .map_err(|_| DecodeGrandpaWarpSyncResponseError)
 }
 
 fn decode_fragments(bytes: &[u8]) -> nom::IResult<&[u8], Vec<GrandpaWarpSyncResponseFragment>> {
@@ -58,17 +96,15 @@ fn decode_fragments(bytes: &[u8]) -> nom::IResult<&[u8], Vec<GrandpaWarpSyncResp
             nom::combinator::map(
                 nom::sequence::tuple((
                     |s| {
-                        crate::header::decode_partial(s)
-                            .map(|(a, b)| (b, a))
-                            .map_err(|_| {
-                                nom::Err::Failure(nom::error::make_error(
-                                    s,
-                                    nom::error::ErrorKind::Verify,
-                                ))
-                            })
+                        header::decode_partial(s).map(|(a, b)| (b, a)).map_err(|_| {
+                            nom::Err::Failure(nom::error::make_error(
+                                s,
+                                nom::error::ErrorKind::Verify,
+                            ))
+                        })
                     },
                     |s| {
-                        crate::finality::justification::decode::decode_partial(s)
+                        finality::justification::decode::decode_partial(s)
                             .map(|(a, b)| (b, a))
                             .map_err(|_| {
                                 nom::Err::Failure(nom::error::make_error(
