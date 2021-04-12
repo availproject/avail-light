333f3ac4b17ea36574adaa1a1de1b52a986c85d3
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-04-12 10:37:33 +0200

    Document identify protocol (#730)
    * Document identify protocol
    
    * Link to specs
    
    * Docfix
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/network/protocol/identify.rs
+++ src/network/protocol/identify.rs
@@ -15,6 +15,21 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//! The identify protocol is a request-response protocol.
+//!
+//! The request's body is empty. Contrary to other request-response protocols, it doesn't even
+//! contain a message length. As soon as the protocol has been negotiated, the other side should
+//! send back the response.
+//!
+//! The response's body consists in various useful general-purpose information about the node.
+//! See [`IdentifyResponse`] for details.
+//!
+//! The two most important fields are [`IdentifyResponse::listen_addrs`] and
+//! [`IdentifyResponse::observed_addr`]. They are necessary in order for nodes to discover their
+//! public address, and in order to insert peers in the Kademlia k-buckets.
+//!
+//! See also [the official specifications](https://github.com/libp2p/specs/tree/69e57d59dc5d59d3979d79842b577ec2c483f7fa/identify).
+
 use super::schema;
 use crate::libp2p::{peer_id::PublicKey, Multiaddr};
 
@@ -29,7 +44,8 @@ pub struct IdentifyResponse<'a, TLaIter, TProtoIter> {
     pub agent_version: &'a str,
     /// Ed25519 public key of the local node.
     pub ed25519_public_key: &'a [u8; 32],
-    /// List of addresses the local node is listening on.
+    /// List of addresses the local node is listening on. This should include first and foremost
+    /// addresses that are publicly-reachable.
     pub listen_addrs: TLaIter,
     /// Address of the sender of the identify request, as seen from the receiver.
     pub observed_addr: &'a Multiaddr,
