00431c611d6e43f2d151531fbcb49101cff72d8c
Anjan Roy <anjanroy@yandex.com>
2021-11-25 16:44:32 +0530

    add: reconstruct column when a subset ( actually >=50% ) of cells are (for certain column) available

--- src/data.rs
+++ src/data.rs
@@ -1,14 +1,19 @@
 extern crate anyhow;
+extern crate dusk_plonk;
 extern crate ipfs_embed;
 extern crate libipld;
 
+use crate::recovery::reconstruct_poly;
 use crate::rpc::get_kate_query_proof_by_cell;
-use crate::types::{BaseCell, DataMatrix, IpldBlock, L0Col, L1Row};
+use crate::types::{BaseCell, Cell, DataMatrix, IpldBlock, L0Col, L1Row};
+use dusk_plonk::bls12_381::BlsScalar;
+use dusk_plonk::fft::EvaluationDomain;
 use ipfs_embed::{Cid, DefaultParams, Ipfs, TempPin};
 use libipld::codec_impl::IpldCodec;
 use libipld::multihash::Code;
 use libipld::Ipld;
 use std::collections::BTreeMap;
+use std::convert::TryInto;
 
 async fn construct_cell(block: u64, row: u16, col: u16) -> BaseCell {
     let data = Ipld::Bytes(get_kate_query_proof_by_cell(block, row, col).await);
@@ -123,4 +128,59 @@ pub async fn push_matrix(
         pin,
     )
     .await?)
+}
+
+// use this function for reconstructing back all cells of certain column
+// when at least 50% of them are available
+//
+// if everything goes fine, returned vector in case of success should have
+// `row_count`-many cells of some specific column, in coded form
+//
+// performing one round of ifft should reveal original data which were
+// coded together
+pub fn reconstruct_column(row_count: usize, cells: &[Cell]) -> Result<Vec<BlsScalar>, String> {
+    // just ensures all rows are from same column !
+    // it's required as that's how it's erasure coded during
+    // construction in validator node
+    fn check_cells(cells: &[Cell]) {
+        assert!(cells.len() > 0);
+        let col = cells[0].col;
+        for cell in cells {
+            assert_eq!(col, cell.col);
+        }
+    }
+
+    // given row index in column of interest, finds it if present
+    // and returns back wrapped in `Some`, otherwise returns `None`
+    fn find_row_by_index(idx: usize, cells: &[Cell]) -> Option<BlsScalar> {
+        for cell in cells {
+            if cell.row == idx as u16 {
+                return Some(
+                    BlsScalar::from_bytes(
+                        &cell.proof[..]
+                            .try_into()
+                            .expect("didn't find u8 array of length 32"),
+                    )
+                    .unwrap(),
+                );
+            }
+        }
+        None
+    }
+
+    // row count of data matrix must be power of two !
+    assert!(row_count & (row_count - 1) == 0);
+    assert!(cells.len() >= row_count / 2 && cells.len() <= row_count);
+    check_cells(cells);
+
+    let eval_domain = EvaluationDomain::new(row_count).unwrap();
+    let mut subset: Vec<Option<BlsScalar>> = Vec::with_capacity(row_count);
+
+    // fill up vector in ordered fashion
+    // @note the way it's done should be improved
+    for i in 0..row_count {
+        subset.push(find_row_by_index(i, cells));
+    }
+
+    reconstruct_poly(eval_domain, subset)
 }
