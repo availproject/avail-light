7c084b063cdd86fb8734ee055afb1ea2113f68a2
Anjan Roy <anjanroy@yandex.com>
2021-12-10 16:18:09 +0530

    fix: data matrix column and cell construction routines don't panic anymore

--- src/data.rs
+++ src/data.rs
@@ -15,37 +15,73 @@ use libipld::Ipld;
 use std::collections::BTreeMap;
 use std::convert::TryInto;
 
-async fn construct_cell(url: &str, block: u64, row: u16, col: u16) -> BaseCell {
-    let data = Ipld::Bytes(get_kate_query_proof_by_cell(url, block, row, col).await);
-    IpldBlock::encode(IpldCodec::DagCbor, Code::Blake3_256, &data).unwrap()
+async fn construct_cell(url: &str, block: u64, row: u16, col: u16) -> Result<BaseCell, String> {
+    match get_kate_query_proof_by_cell(url, block, row, col).await {
+        Ok(cell) => {
+            let data = Ipld::Bytes(cell);
+            match IpldBlock::encode(IpldCodec::DagCbor, Code::Blake3_256, &data) {
+                Ok(coded_cell) => Ok(coded_cell),
+                Err(_) => Err("failed to IPLD encode cell of data matrix".to_owned()),
+            }
+        }
+        Err(msg) => Err(msg),
+    }
 }
 
-async fn construct_colwise(url: &str, block: u64, row_count: u16, col: u16) -> L0Col {
+async fn construct_colwise(
+    url: &str,
+    block: u64,
+    row_count: u16,
+    col: u16,
+) -> Result<L0Col, String> {
     let mut base_cells: Vec<BaseCell> = Vec::with_capacity(row_count as usize);
 
     for row in 0..row_count {
-        base_cells.push(construct_cell(url, block, row, col).await);
+        let cell = match construct_cell(url, block, row, col).await {
+            Ok(cell) => {
+                base_cells.push(cell);
+            }
+            Err(msg) => return Err(msg),
+        };
     }
 
-    L0Col {
+    Ok(L0Col {
         base_cells: base_cells,
-    }
+    })
 }
 
-async fn construct_rowwise(url: &str, block: u64, row_count: u16, col_count: u16) -> L1Row {
+async fn construct_rowwise(
+    url: &str,
+    block: u64,
+    row_count: u16,
+    col_count: u16,
+) -> Result<L1Row, String> {
     let mut l0_cols: Vec<L0Col> = Vec::with_capacity(col_count as usize);
 
     for col in 0..col_count {
-        l0_cols.push(construct_colwise(url, block, row_count, col).await);
+        match construct_colwise(url, block, row_count, col).await {
+            Ok(col) => {
+                l0_cols.push(col);
+            }
+            Err(msg) => return Err(msg),
+        };
     }
 
-    L1Row { l0_cols: l0_cols }
+    Ok(L1Row { l0_cols: l0_cols })
 }
 
-pub async fn construct_matrix(url: &str, block: u64, row_count: u16, col_count: u16) -> DataMatrix {
-    DataMatrix {
-        l1_row: construct_rowwise(url, block, row_count, col_count).await,
-        block_num: block as i128,
+pub async fn construct_matrix(
+    url: &str,
+    block: u64,
+    row_count: u16,
+    col_count: u16,
+) -> Result<DataMatrix, String> {
+    match construct_rowwise(url, block, row_count, col_count).await {
+        Ok(row) => Ok(DataMatrix {
+            l1_row: row,
+            block_num: block as i128,
+        }),
+        Err(msg) => return Err(msg),
     }
 }
 
@@ -53,29 +89,43 @@ async fn push_cell(
     cell: BaseCell,
     ipfs: &Ipfs<DefaultParams>,
     pin: &TempPin,
-) -> anyhow::Result<Cid> {
-    ipfs.temp_pin(pin, cell.cid())?;
-    ipfs.insert(&cell)?;
-
-    Ok(*cell.cid())
+) -> anyhow::Result<Cid, String> {
+    match ipfs.temp_pin(pin, cell.cid()) {
+        Ok(_) => match ipfs.insert(&cell) {
+            Ok(_) => Ok(*cell.cid()),
+            Err(_) => Err("failed to IPFS insert cell of data matrix".to_owned()),
+        },
+        Err(_) => Err("failed to IPFS pin cell of data matrix".to_owned()),
+    }
 }
 
-async fn push_col(col: L0Col, ipfs: &Ipfs<DefaultParams>, pin: &TempPin) -> anyhow::Result<Cid> {
+async fn push_col(
+    col: L0Col,
+    ipfs: &Ipfs<DefaultParams>,
+    pin: &TempPin,
+) -> anyhow::Result<Cid, String> {
     let mut cell_cids: Vec<Ipld> = Vec::with_capacity(col.base_cells.len());
 
     for cell in col.base_cells {
-        if let Ok(cid) = push_cell(cell, ipfs, pin).await {
-            cell_cids.push(Ipld::Link(cid));
+        match push_cell(cell, ipfs, pin).await {
+            Ok(cid) => {
+                cell_cids.push(Ipld::Link(cid));
+            }
+            Err(msg) => return Err(msg),
         };
     }
 
     let col = Ipld::List(cell_cids);
-    let coded_col = IpldBlock::encode(IpldCodec::DagCbor, Code::Blake3_256, &col).unwrap();
-
-    ipfs.temp_pin(pin, coded_col.cid())?;
-    ipfs.insert(&coded_col)?;
-
-    Ok(*coded_col.cid())
+    match IpldBlock::encode(IpldCodec::DagCbor, Code::Blake3_256, &col) {
+        Ok(coded_col) => match ipfs.temp_pin(pin, coded_col.cid()) {
+            Ok(_) => match ipfs.insert(&coded_col) {
+                Ok(_) => Ok(*coded_col.cid()),
+                Err(_) => Err("failed to IPFS insert column of data matrix".to_owned()),
+            },
+            Err(_) => Err("failed to IPFS pin column of data matrix".to_owned()),
+        },
+        Err(_) => Err("failed to IPLD encode column of data matrix".to_owned()),
+    }
 }
 
 async fn push_row(
