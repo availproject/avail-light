64c023fcf0f9bf4dabc88cce3dd6ecd8b48c93a2
Aleksandar Terentić <aleksandar@ethernal.tech>
2022-06-02 11:10:06 +0200

    Store decoded data into database.

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,4 @@
 pub const CONFIDENCE_FACTOR_CF: &str = "avail_light_confidence_factor_cf";
 pub const BLOCK_HEADER_CF: &str = "avail_light_block_header_cf";
 pub const BLOCK_CID_CF: &str = "avail_light_block_cid_cf";
+pub const APP_DATA_CF: &str = "avail_light_app_data_cf";
