c0b21939edcd0cd1b392c9efc52bc01806d5c818
Ljubisa Isakovic <93769705+sh3ll3x3c@users.noreply.github.com>
2022-08-02 15:23:13 +0200

    Implement a more generic way of metrics handling on LC (#104)

--- src/prometheus_handler.rs
+++ src/prometheus_handler.rs
@@ -16,8 +16,13 @@ pub use prometheus::{
 	exponential_buckets, Error as PrometheusError, Histogram, HistogramOpts, HistogramVec, Opts,
 	Registry,
 };
-use prometheus::{Encoder, TextEncoder};
+use prometheus::{core::Collector, Encoder, TextEncoder};
 use tracing::info;
+
+pub fn register<T: Clone + Collector + 'static>(metric: T, registry: &Registry) -> Result<T> {
+	registry.register(Box::new(metric.clone()))?;
+	Ok(metric)
+}
 
 async fn request_metrics(req: Request<Body>, registry: Registry) -> Result<Response<Body>> {
 	if req.uri().path() == "/metrics" {
