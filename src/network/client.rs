e17376f506fd0d9fbb9226f41c930c18bb265b3e
momosh <momcilo@ethernal.tech>
2022-11-28 12:38:31 +0100

    added client mod

---
+++ src/network/client.rs
@@ -0,0 +1,127 @@
+use anyhow::{Context, Result};
+use libp2p::{
+	kad::{record::Key, PeerRecord, Quorum, Record},
+	Multiaddr, PeerId,
+};
+use tokio::sync::{mpsc, oneshot};
+
+#[derive(Clone)]
+pub struct Client {
+	sender: mpsc::Sender<Command>,
+}
+
+impl Client {
+	pub fn new(sender: mpsc::Sender<Command>) -> Self {
+		Self { sender }
+	}
+
+	pub async fn start_listening(&self, addr: Multiaddr) -> Result<(), anyhow::Error> {
+		let (sender, receiver) = oneshot::channel();
+		self.sender
+			.send(Command::StartListening { addr, sender })
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+
+	pub async fn add_address(
+		&self,
+		peer_id: PeerId,
+		peer_addr: Multiaddr,
+	) -> Result<(), anyhow::Error> {
+		let (sender, receiver) = oneshot::channel();
+		self.sender
+			.send(Command::AddAddress {
+				peer_id,
+				peer_addr,
+				sender,
+			})
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+
+	pub async fn dial(&self, peer_id: PeerId, peer_addr: Multiaddr) -> Result<()> {
+		let (sender, receiver) = oneshot::channel();
+		self.sender
+			.send(Command::Dial {
+				peer_id,
+				peer_addr,
+				sender,
+			})
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+
+	pub async fn bootstrap(&self, nodes: Vec<(PeerId, Multiaddr)>) -> Result<()> {
+		let (sender, receiver) = oneshot::channel();
+		for (peer, addr) in nodes {
+			self.add_address(peer, addr.clone()).await?;
+			self.dial(peer, addr).await?;
+		}
+
+		self.sender
+			.send(Command::Bootstrap { sender })
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+
+	pub async fn get_kad_record(&self, key: Key, quorum: Quorum) -> Result<Vec<PeerRecord>> {
+		let (sender, receiver) = oneshot::channel();
+		self.sender
+			.send(Command::GetKadRecord {
+				key,
+				quorum,
+				sender,
+			})
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+
+	pub async fn put_kad_record(&self, record: Record, quorum: Quorum) -> Result<()> {
+		let (sender, receiver) = oneshot::channel();
+		self.sender
+			.send(Command::PutKadRecord {
+				record,
+				quorum,
+				sender,
+			})
+			.await
+			.context("Command receiver should not be dropped.")?;
+		receiver.await.context("Sender not to be dropped.")?
+	}
+}
+
+#[derive(Debug)]
+pub enum Command {
+	StartListening {
+		addr: Multiaddr,
+		sender: oneshot::Sender<Result<()>>,
+	},
+	AddAddress {
+		peer_id: PeerId,
+		peer_addr: Multiaddr,
+		sender: oneshot::Sender<Result<()>>,
+	},
+	Dial {
+		peer_id: PeerId,
+		peer_addr: Multiaddr,
+		sender: oneshot::Sender<Result<()>>,
+	},
+	Bootstrap {
+		sender: oneshot::Sender<Result<()>>,
+	},
+	GetKadRecord {
+		key: Key,
+		quorum: Quorum,
+		sender: oneshot::Sender<Result<Vec<PeerRecord>>>,
+	},
+	PutKadRecord {
+		record: Record,
+		quorum: Quorum,
+		sender: oneshot::Sender<Result<()>>,
+	},
+}
