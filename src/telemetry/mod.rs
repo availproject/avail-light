f300e58336ed8525ceda055f37dd82d160c2cb61
Aleksandar Terentić <aleksandar@ethernal.tech>
2023-02-08 14:28:24 +0100

    Split binding and starting prometheus server.

--- src/telemetry/mod.rs
+++ src/telemetry/mod.rs
@@ -1,23 +1,29 @@
-use std::{net::SocketAddr, sync::Arc};
-
 use anyhow::{Error, Ok};
 use hyper::{
 	server::conn::AddrStream,
 	service::{make_service_fn, service_fn},
 	Server,
 };
-use prometheus_client::registry::Registry;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::Mutex;
+
+use prometheus_client::registry::Registry;
 use tracing::info;
 
 pub mod handler;
 pub mod metrics;
 
-pub async fn http_server(prometheus_addr: SocketAddr, registry: Registry) -> Result<(), Error> {
+pub async fn bind(prometheus_addr: SocketAddr) -> Result<hyper::server::conn::AddrIncoming, Error> {
 	let listener = tokio::net::TcpListener::bind(&prometheus_addr)
 		.await
 		.map_err(|_| anyhow::anyhow!("Port already in use: {}", prometheus_addr))?;
+	hyper::server::conn::AddrIncoming::from_listener(listener).map_err(Into::into)
+}
 
+pub async fn http_server(
+	incoming: hyper::server::conn::AddrIncoming,
+	registry: Registry,
+) -> Result<(), Error> {
 	let context = Arc::new(Mutex::new(registry));
 
 	let service = make_service_fn(move |_conn: &AddrStream| {
@@ -26,10 +32,8 @@ pub async fn http_server(prometheus_addr: SocketAddr, registry: Registry) -> Res
 		async move { Ok(service) }
 	});
 
-	let incoming = hyper::server::conn::AddrIncoming::from_listener(listener)?;
 	let server = Server::builder(incoming).serve(service);
 	info!("Metrics server on http://{}/metrics", server.local_addr());
 
-	let result = server.await.map_err(Into::into);
-	result
+	server.await.map_err(Into::into)
 }
