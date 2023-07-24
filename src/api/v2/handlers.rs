cf3c416431093ee6debfd691dd80ab8a05c39bf6
Aleksandar Terentić <aleksandar@ethernal.tech>
2023-07-24 15:37:14 +0200

    Add subscriptions endpoint.

--- src/api/v2/handlers.rs
+++ src/api/v2/handlers.rs
@@ -1,8 +1,20 @@
-use super::types::Version;
+use super::types::{Client, Clients, Subscription, SubscriptionId, Version};
+use std::convert::Infallible;
+use uuid::Uuid;
 
 pub fn version(version: String, network_version: String) -> Version {
 	Version {
 		version,
 		network_version,
 	}
+}
+
+pub async fn subscriptions(
+	subscription: Subscription,
+	clients: Clients,
+) -> Result<SubscriptionId, Infallible> {
+	let subscription_id = Uuid::new_v4().to_string();
+	let mut clients = clients.write().await;
+	clients.insert(subscription_id.clone(), Client { subscription });
+	Ok(SubscriptionId { subscription_id })
 }
