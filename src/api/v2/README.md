9dad936455822a85a63e7e0cdf33de3ecf813aaf
Aleksandar Terentić <aleksandar@ethernal.tech>
2023-07-24 15:37:14 +0200

    Add subscriptions endpoint.

--- src/api/v2/README.md
+++ src/api/v2/README.md
@@ -33,3 +33,46 @@ Content-Type: application/json
 
 - **version** - the Avail Light Client version
 - **network_version** - Avail network version supported by the Avail Light Client
+
+# WebSocket API
+
+The Avail Light Client WebSocket API allows real-time communication between a client and a server over a persistent connection, enabling push notifications as an alternative to polling. Web socket API can be used on its own or in combination with HTTP API to enable different pull/push use cases.
+
+## POST `/v2/subscriptions`
+
+Creates subscriptions for given topics. In case of reconnects, the user needs to subscribe again.
+
+Request:
+
+```yaml
+POST /v2/subscriptions HTTP/1.1
+Host: {light-client-url}
+Content-Type: application/json
+Content-Length: {content-length}
+
+{
+  "topics": ["header-verified", "confidence-achieved", "data-verified"],
+  "data_fields": ["data", "raw"]
+}
+```
+
+Response:
+
+```yaml
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+  "subscription_id": "{subscription-id}"
+}
+```
+
+## Topics
+
+- **header-verified** - header finality is verified and header is available
+- **confidence-achieved** - confidence is achieved
+- **data-verified** - block data is verified and available
+
+### Data fields
+
+Filters **data-verified** message. Optional parameter used when **raw** transaction data is needed. If omitted, only decoded **data** is present in the message.
--- src/api/v2/mod.rs
+++ src/api/v2/mod.rs
@@ -1,7 +1,24 @@
+use self::types::Clients;
+use std::{collections::HashMap, convert::Infallible, sync::Arc};
+use tokio::sync::RwLock;
 use warp::{Filter, Rejection, Reply};
 
 mod handlers;
 mod types;
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
+	warp::any().map(move || clients.clone())
+}
+
+fn subscriptions_route(
+	clients: Clients,
+) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+	warp::path!("v2" / "subscriptions")
+		.and(warp::post())
+		.and(warp::body::json())
+		.and(with_clients(clients))
+		.and_then(handlers::subscriptions)
+}
 
 fn version_route(
 	version: String,
@@ -18,11 +35,20 @@ pub fn routes(
 	version: String,
 	network_version: String,
 ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-	version_route(version, network_version)
+	let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
+	version_route(version, network_version).or(subscriptions_route(clients))
 }
 
 #[cfg(test)]
 mod tests {
+	use crate::api::v2::types::{Clients, DataFields, Subscription, SubscriptionId, Topics};
+	use std::{
+		collections::{HashMap, HashSet},
+		str::FromStr,
+		sync::Arc,
+	};
+	use tokio::sync::RwLock;
+
 	#[tokio::test]
 	async fn version_route() {
 		let route = super::version_route("v1.0.0".to_string(), "nv1.0.0".to_string());
@@ -36,5 +62,46 @@ mod tests {
 			response.body(),
 			r#"{"version":"v1.0.0","network_version":"nv1.0.0"}"#
 		);
+	}
+
+	fn all_topics() -> HashSet<Topics> {
+		vec![
+			Topics::HeaderVerified,
+			Topics::ConfidenceAchieved,
+			Topics::DataVerified,
+		]
+		.into_iter()
+		.collect()
+	}
+
+	fn all_data_fields() -> HashSet<DataFields> {
+		vec![DataFields::Raw, DataFields::Data]
+			.into_iter()
+			.collect()
+	}
+
+	#[tokio::test]
+	async fn subscriptions_route() {
+		let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
+		let route = super::subscriptions_route(clients.clone());
+
+		let body = r#"{"topics":["confidence-achieved","data-verified","header-verified"],"data_fields":["data","raw"]}"#;
+		let response = warp::test::request()
+			.method("POST")
+			.body(body)
+			.path("/v2/subscriptions")
+			.reply(&route)
+			.await;
+
+		let SubscriptionId { subscription_id } = serde_json::from_slice(response.body()).unwrap();
+		assert!(uuid::Uuid::from_str(&subscription_id).is_ok());
+
+		let clients = clients.read().await;
+		let client = clients.get(&subscription_id).unwrap();
+		let expected = Subscription {
+			topics: all_topics(),
+			data_fields: all_data_fields(),
+		};
+		assert!(client.subscription == expected);
 	}
 }
