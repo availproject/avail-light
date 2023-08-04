c915a76307bd43bfa570ffdf2392c09da2405bd9
Luka Borkovic <luka.borkovic@ethernal.tech>
2023-08-04 13:18:52 +0200

    Switch main to a separate binary.

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod api;
+pub mod app_client;
+pub mod consts;
+pub mod data;
+pub mod light_client;
+pub mod network;
+pub mod proof;
+pub mod rpc;
+pub mod subscriptions;
+pub mod sync_client;
+pub mod telemetry;
+pub mod types;
+pub mod utils;
