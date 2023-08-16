d0c5ed24b76e6a79e192233c5a79f4e205e8dd05
Aleksandar Terentić <aleksandar@ethernal.tech>
2023-08-16 12:35:22 +0200

    Handle web socket status request.

--- src/api/v2/mod.rs
+++ src/api/v2/mod.rs
@@ -58,11 +58,17 @@ fn subscriptions_route(
 fn ws_route(
 	clients: Clients,
 	version: Version,
+	config: RuntimeConfig,
+	node: Node,
+	state: Arc<Mutex<State>>,
 ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
 	warp::path!("v2" / "ws" / String)
 		.and(warp::ws())
 		.and(with_clients(clients))
 		.and(warp::any().map(move || version.clone()))
+		.and(warp::any().map(move || config.clone()))
+		.and(warp::any().map(move || node.clone()))
+		.and(warp::any().map(move || state.clone()))
 		.and_then(handlers::ws)
 }
 
@@ -79,9 +85,9 @@ pub fn routes(
 		network_version,
 	};
 	version_route(version.clone())
-		.or(status_route(config, node, state))
+		.or(status_route(config.clone(), node.clone(), state.clone()))
 		.or(subscriptions_route(clients.clone()))
-		.or(ws_route(clients, version))
+		.or(ws_route(clients, version, config, node, state))
 		.recover(handle_rejection)
 }
 
@@ -102,6 +108,7 @@ mod tests {
 	};
 	use tokio::sync::RwLock;
 	use uuid::Uuid;
+	use warp::test::WsClient;
 
 	fn v1() -> Version {
 		Version {
@@ -247,16 +254,26 @@ mod tests {
 		(uuid, clients)
 	}
 
-	#[tokio::test]
-	async fn ws_route_version() {
-		let (uuid, clients) = init_clients().await;
-
-		let route = super::ws_route(clients.clone(), v1());
-		let mut client = warp::test::ws()
+	async fn init_ws_client(
+		uuid: Uuid,
+		clients: Clients,
+		state: Arc<Mutex<State>>,
+		config: RuntimeConfig,
+	) -> WsClient {
+		let route = super::ws_route(clients.clone(), v1(), config, Node::default(), state);
+		warp::test::ws()
 			.path(&format!("/v2/ws/{uuid}"))
 			.handshake(route)
 			.await
-			.expect("handshake");
+			.expect("handshake")
+	}
+
+	#[tokio::test]
+	async fn ws_route_version() {
+		let (uuid, clients) = init_clients().await;
+		let state = Arc::new(Mutex::new(State::default()));
+		let config = RuntimeConfig::default();
+		let mut client = init_ws_client(uuid, clients, state, config).await;
 
 		client
 			.send_text(r#"{"type":"version","request_id":"1"}"#)
@@ -268,10 +285,60 @@ mod tests {
 	}
 
 	#[tokio::test]
+	async fn ws_route_status() {
+		let (uuid, clients) = init_clients().await;
+		let state = Arc::new(Mutex::new(State::default()));
+		let config = RuntimeConfig {
+			app_id: Some(1),
+			sync_start_block: Some(10),
+			block_matrix_partition: Some(Partition {
+				number: 1,
+				fraction: 10,
+			}),
+			..Default::default()
+		};
+		let mut client = init_ws_client(uuid, clients, state.clone(), config).await;
+
+		{
+			let mut state = state.lock().unwrap();
+			state.latest = 30;
+			state.set_confidence_achieved(20);
+			state.set_confidence_achieved(29);
+			state.set_data_verified(20);
+			state.set_data_verified(29);
+			state.set_synced(false);
+			state.set_sync_confidence_achieved(10);
+			state.set_sync_confidence_achieved(19);
+			state.set_sync_data_verified(10);
+			state.set_sync_data_verified(18);
+		}
+
+		client
+			.send_text(r#"{"type":"status","request_id":"1"}"#)
+			.await;
+
+		let message = format!(
+			r#"{{"modes":["light","app","partition"],"app_id":1,"genesis_hash":"{GENESIS_HASH}","network":"{NETWORK}","blocks":{{"latest":30,"available":{{"first":20,"last":29}},"app_data":{{"first":20,"last":29}},"historical_sync":{{"synced":false,"available":{{"first":10,"last":19}},"app_data":{{"first":10,"last":18}}}}}},"partition":"1/10"}}"#
+		);
+
+		let expected = format!(r#"{{"topic":"status","request_id":"1","message":{message}}}"#);
+		let message = client.recv().await.unwrap();
+		assert_eq!(expected, message.to_str().unwrap());
+	}
+
+	#[tokio::test]
 	async fn ws_route_bad_request() {
 		let (uuid, clients) = init_clients().await;
+		let state = Arc::new(Mutex::new(State::default()));
 
-		let route = super::ws_route(clients.clone(), v1());
+		let route = super::ws_route(
+			clients.clone(),
+			v1(),
+			RuntimeConfig::default(),
+			Node::default(),
+			state,
+		);
+
 		let mut client = warp::test::ws()
 			.path(&format!("/v2/ws/{uuid}"))
 			.handshake(route)
