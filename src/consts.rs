0daf8149803b676b23801ad369da49de6b3bfb2c
momosh <momcilo@ethernal.tech>
2024-01-25 16:40:56 +0100

    new constants config

--- src/consts.rs
+++ src/consts.rs
@@ -12,6 +12,18 @@ pub const APP_DATA_CF: &str = "avail_light_app_data_cf";
 /// Column family for state
 pub const STATE_CF: &str = "avail_light_state_cf";
 
-/// Expected network versions
+/// Expected network Node versions
 pub const EXPECTED_SYSTEM_VERSION: &str = "1.9";
 pub const EXPECTED_SPEC_NAME: &str = "data-avail";
+pub struct ExpectedNodeVariant {
+	pub system_version: &'static str,
+	pub spec_name: &'static str,
+}
+impl ExpectedNodeVariant {
+	pub const fn new() -> Self {
+		Self {
+			system_version: EXPECTED_SYSTEM_VERSION,
+			spec_name: EXPECTED_SPEC_NAME,
+		}
+	}
+}
