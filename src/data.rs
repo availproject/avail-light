8b399ea88d5b4cacbf7087c7d697b3542f2dc0eb
momosh <momcilo@ethernal.tech>
2024-02-28 20:18:01 +0100

    new database trait with new data manager

--- src/data.rs
+++ src/data.rs
@@ -211,3 +211,107 @@ pub fn store_finality_sync_checkpoint(
 	)
 	.wrap_err("Failed to write finality sync checkpoint data")
 }
+
+pub trait Database: Send + Sync {
+	fn put(&self, column_family: Option<&str>, key: &[u8], value: &[u8]) -> Result<()>;
+	fn get(&self, column_family: Option<&str>, key: &[u8]) -> Result<Option<Vec<u8>>>;
+	fn delete(&self, column_family: Option<&str>, key: &[u8]) -> Result<()>;
+}
+
+pub struct RocksDB {
+	db: Arc<rocksdb::DB>,
+}
+
+impl RocksDB {
+	pub fn open(path: &str) -> Result<RocksDB> {
+		let cf_opts = vec![
+			ColumnFamilyDescriptor::new(CONFIDENCE_FACTOR_CF, Options::default()),
+			ColumnFamilyDescriptor::new(BLOCK_HEADER_CF, Options::default()),
+			ColumnFamilyDescriptor::new(APP_DATA_CF, Options::default()),
+			ColumnFamilyDescriptor::new(STATE_CF, Options::default()),
+		];
+
+		let mut db_opts = Options::default();
+		db_opts.create_if_missing(true);
+		db_opts.create_missing_column_families(true);
+
+		let db = rocksdb::DB::open_cf_descriptors(&db_opts, path, cf_opts)?;
+		Ok(RocksDB { db: Arc::new(db) })
+	}
+}
+
+impl Database for RocksDB {
+	fn put(&self, column_family: Option<&str>, key: &[u8], value: &[u8]) -> Result<()> {
+		// if Column Family descriptor was provided, put the key in that partition
+		let Some(cf) = column_family else {
+			// else, just put it in the default partition
+			return self
+				.db
+				.put(key, value)
+				.wrap_err("Put operation failed on RocksDB");
+		};
+		let cf_handle = self
+			.db
+			.cf_handle(cf)
+			.ok_or_else(|| eyre!("Couldn't get Column Family handle from RocksDB"))?;
+		self.db
+			.put_cf(&cf_handle, key, value)
+			.wrap_err("Put operation with Column Family failed on RocksDB")
+	}
+
+	fn get(&self, column_family: Option<&str>, key: &[u8]) -> Result<Option<Vec<u8>>> {
+		// if Column Family descriptor was provided, get the key from that partition
+		let Some(cf) = column_family else {
+			// else, just get it from the default partition
+			return self.db.get(key).wrap_err("Get operation failed on RocksDB");
+		};
+		let cf_handle = self
+			.db
+			.cf_handle(cf)
+			.ok_or_else(|| eyre!("Couldn't get Column Family handle from RocksDB"))?;
+		self.db
+			.get_cf(&cf_handle, key)
+			.wrap_err("Get operation with Column Family failed on RocksDB")
+	}
+
+	fn delete(&self, column_family: Option<&str>, key: &[u8]) -> Result<()> {
+		// if Column Family descriptor was provided, delete the key from that partition
+		let Some(cf) = column_family else {
+			// else, just delete it from the default partition
+			return self
+				.db
+				.delete(key)
+				.wrap_err("Delete operation failed on RocksDB");
+		};
+		let cf_handle = self
+			.db
+			.cf_handle(cf)
+			.ok_or_else(|| eyre!("Couldn't get Column Family handle from RocksDB"))?;
+		self.db
+			.delete_cf(&cf_handle, key)
+			.wrap_err("Delete operation with Column Family failed on RocksDB")
+	}
+}
+
+// Generic struct that uses any type implementing the Database trait
+#[derive(Clone)]
+pub struct DataManager<T: Database + Clone> {
+	db: T,
+}
+
+impl<T: Database + Clone> DataManager<T> {
+	pub fn new(db: T) -> DataManager<T> {
+		DataManager { db }
+	}
+
+	pub fn store_extrinsics<D: Encode>(
+		&self,
+		app_id: AppId,
+		block_number: u32,
+		data: &D,
+	) -> Result<()> {
+		let key = format!("{}:{block_number}", app_id.0);
+		self.db
+			.put(Some(APP_DATA_CF), key.as_bytes(), &data.encode())
+	}
+}
