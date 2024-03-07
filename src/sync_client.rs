13f9278bb2afebaf607879eaef1c747241fac23f
momosh <momcilo@ethernal.tech>
2024-03-07 21:23:07 +0100

    enabled mocks and tests

--- src/sync_client.rs
+++ src/sync_client.rs
@@ -26,6 +26,7 @@ use crate::{
 	utils::{calculate_confidence, extract_app_lookup, extract_kate},
 };
 
+use async_trait::async_trait;
 use avail_subxt::{primitives::Header as DaHeader, utils::H256};
 use codec::Encode;
 use color_eyre::{
@@ -33,6 +34,7 @@ use color_eyre::{
 	Result,
 };
 use kate_recovery::{commitments, matrix::Dimensions};
+use mockall::automock;
 use sp_core::blake2_256;
 use std::{
 	ops::Range,
@@ -42,17 +44,28 @@ use std::{
 use tokio::sync::broadcast;
 use tracing::{error, info, warn};
 
+#[async_trait]
+#[automock]
+pub trait Client {
+	async fn get_header_by_block_number(&self, block_number: u32) -> Result<(DaHeader, H256)>;
+	fn is_confidence_stored(&self, block_number: u32) -> Result<bool>;
+	fn store_confidence(&self, count: u32, block_number: u32) -> Result<()>;
+}
+
 #[derive(Clone)]
-pub struct SyncClient<T: Database + Clone> {
+pub struct SyncClient<T: Database + Sync> {
 	db: T,
 	rpc_client: RpcClient,
 }
 
-pub fn new<T: Database + Clone>(db: T, rpc_client: RpcClient) -> SyncClient<T> {
-	SyncClient { db, rpc_client }
+impl<T: Database + Sync> SyncClient<T> {
+	pub fn new(db: T, rpc_client: RpcClient) -> impl Client {
+		SyncClient { db, rpc_client }
+	}
 }
 
-impl<T: Database + Clone> SyncClient<T> {
+#[async_trait]
+impl<T: Database + Sync> Client for SyncClient<T> {
 	async fn get_header_by_block_number(&self, block_number: u32) -> Result<(DaHeader, H256)> {
 		if let Some(header) = self
 			.db
@@ -98,8 +111,8 @@ impl<T: Database + Clone> SyncClient<T> {
 	}
 }
 
-async fn process_block<T: Database + Clone>(
-	sync_client: SyncClient<T>,
+async fn process_block(
+	client: &impl Client,
 	network_client: &impl network::Client,
 	header: DaHeader,
 	header_hash: H256,
@@ -139,9 +152,7 @@ async fn process_block<T: Database + Clone>(
 	}
 
 	// write confidence factor into on-disk database
-	sync_client
-		.store_confidence(fetched.len().try_into()?, block_number)
-		.wrap_err("Failed to store confidence in DB")?;
+	client.store_confidence(fetched.len().try_into()?, block_number)?;
 
 	let confidence = Some(calculate_confidence(fetched.len() as u32));
 	let client_msg =
@@ -162,8 +173,8 @@ async fn process_block<T: Database + Clone>(
 /// * `start_block` - Sync start block
 /// * `end_block` - Sync end block
 /// * `block_verified_sender` - Optional channel to send verified blocks
-pub async fn run<T: Database + Clone>(
-	sync_client: SyncClient<T>,
+pub async fn run(
+	client: impl Client,
 	network_client: impl network::Client,
 	cfg: SyncClientConfig,
 	sync_range: Range<u32>,
@@ -183,7 +194,7 @@ pub async fn run<T: Database + Clone>(
 	for block_number in sync_range {
 		// TODO: This is still an ambiguous check since data fetch can fail.
 		// We should write block status in DB explicitly.
-		match sync_client.is_confidence_stored(block_number) {
+		match client.is_confidence_stored(block_number) {
 			Ok(false) => (),
 			Ok(true) => continue,
 			Err(error) => {
@@ -193,8 +204,7 @@ pub async fn run<T: Database + Clone>(
 			},
 		};
 
-		let (header, header_hash) = match sync_client.get_header_by_block_number(block_number).await
-		{
+		let (header, header_hash) = match client.get_header_by_block_number(block_number).await {
 			Ok(value) => value,
 			Err(error) => {
 				error!(block_number, "Cannot process block: {error:#}");
@@ -212,7 +222,7 @@ pub async fn run<T: Database + Clone>(
 		// TODO: Should we handle unprocessed blocks differently?
 		let block_verified_sender = block_verified_sender.clone();
 		if let Err(error) = process_block(
-			sync_client.clone(),
+			&client,
 			&network_client,
 			header,
 			header_hash,
@@ -239,10 +249,7 @@ mod tests {
 	use std::time::Duration;
 
 	use super::*;
-	use crate::{
-		data::mem_db,
-		types::{self, RuntimeConfig},
-	};
+	use crate::types::{self, RuntimeConfig};
 	use avail_subxt::{
 		api::runtime_types::avail_core::{
 			data_lookup::compact::CompactDataLookup,
@@ -292,179 +299,178 @@ mod tests {
 		}
 	}
 
-	// #[tokio::test]
-	// pub async fn test_process_blocks_without_rpc() {
-	// 	let (block_tx, _) = broadcast::channel::<types::BlockVerified>(10);
-	// 	let mut cfg = SyncClientConfig::from(&RuntimeConfig::default());
-	// 	cfg.disable_rpc = true;
-	// 	let mut mock_network_client = network::MockClient::new();
-	// 	let mut mock_client = MockSyncClient::new();
-	// 	let header = default_header();
-	// 	let header_hash: H256 =
-	// 		hex!("3767f8955d6f7306b1e55701b6316fa1163daa8d4cffdb05c3b25db5f5da1723").into();
+	#[tokio::test]
+	pub async fn test_process_blocks_without_rpc() {
+		let (block_tx, _) = broadcast::channel::<types::BlockVerified>(10);
+		let mut cfg = SyncClientConfig::from(&RuntimeConfig::default());
+		cfg.disable_rpc = true;
+		let mut mock_network_client = network::MockClient::new();
+		let mut mock_client = MockClient::new();
+		let header = default_header();
+		let header_hash: H256 =
+			hex!("3767f8955d6f7306b1e55701b6316fa1163daa8d4cffdb05c3b25db5f5da1723").into();
 
-	// 	mock_network_client
-	// 		.expect_fetch_verified()
-	// 		.returning(move |_, _, _, _, positions| {
-	// 			let unfetched = vec![];
-	// 			let fetched: Vec<Cell> = vec![
-	// 				Cell {
-	// 					position: Position { row: 0, col: 0 },
-	// 					content: [
-	// 						183, 56, 112, 134, 157, 186, 15, 255, 245, 173, 188, 37, 165, 224, 226,
-	// 						80, 196, 137, 235, 233, 154, 4, 110, 142, 26, 95, 150, 132, 61, 23,
-	// 						202, 212, 101, 6, 235, 6, 102, 188, 206, 147, 36, 121, 128, 63, 240,
-	// 						37, 200, 236, 4, 44, 40, 4, 3, 0, 11, 35, 249, 222, 81, 135, 1, 128, 0,
-	// 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	// 					],
-	// 				},
-	// 				Cell {
-	// 					position: Position { row: 0, col: 2 },
-	// 					content: [
-	// 						153, 31, 34, 70, 221, 239, 97, 236, 3, 172, 44, 167, 114, 117, 186,
-	// 						245, 171, 12, 70, 144, 204, 207, 82, 160, 29, 83, 245, 203, 40, 238,
-	// 						96, 131, 68, 96, 9, 136, 151, 88, 218, 72, 79, 55, 193, 228, 71, 193,
-	// 						120, 113, 48, 237, 151, 135, 246, 8, 251, 150, 106, 44, 29, 250, 250,
-	// 						54, 133, 203, 162, 73, 252, 32, 42, 175, 24, 166, 142, 72, 226, 150,
-	// 						163, 206, 115, 0,
-	// 					],
-	// 				},
-	// 				Cell {
-	// 					position: Position { row: 1, col: 1 },
-	// 					content: [
-	// 						146, 211, 61, 65, 166, 68, 252, 65, 196, 167, 211, 64, 223, 151, 33,
-	// 						133, 67, 132, 59, 13, 224, 100, 55, 104, 180, 174, 17, 41, 151, 125,
-	// 						193, 80, 142, 140, 216, 97, 117, 60, 217, 44, 242, 7, 30, 204, 22, 197,
-	// 						12, 179, 88, 163, 102, 4, 54, 208, 14, 161, 193, 25, 34, 179, 35, 234,
-	// 						120, 131, 62, 53, 0, 54, 72, 49, 196, 234, 239, 65, 25, 159, 245, 38,
-	// 						193, 0,
-	// 					],
-	// 				},
-	// 				Cell {
-	// 					position: Position { row: 0, col: 3 },
-	// 					content: [
-	// 						150, 6, 83, 12, 56, 17, 0, 225, 186, 238, 151, 181, 116, 1, 34, 240,
-	// 						174, 192, 98, 201, 60, 208, 50, 215, 90, 231, 2, 27, 17, 204, 140, 30,
-	// 						213, 253, 200, 176, 72, 98, 121, 25, 239, 76, 230, 154, 121, 246, 142,
-	// 						37, 85, 184, 201, 218, 107, 88, 0, 87, 199, 169, 98, 172, 4, 140, 151,
-	// 						65, 162, 162, 190, 205, 20, 95, 67, 114, 73, 59, 170, 52, 243, 140,
-	// 						237, 0,
-	// 					],
-	// 				},
-	// 			];
+		mock_network_client
+			.expect_fetch_verified()
+			.returning(move |_, _, _, _, positions| {
+				let unfetched = vec![];
+				let fetched: Vec<Cell> = vec![
+					Cell {
+						position: Position { row: 0, col: 0 },
+						content: [
+							183, 56, 112, 134, 157, 186, 15, 255, 245, 173, 188, 37, 165, 224, 226,
+							80, 196, 137, 235, 233, 154, 4, 110, 142, 26, 95, 150, 132, 61, 23,
+							202, 212, 101, 6, 235, 6, 102, 188, 206, 147, 36, 121, 128, 63, 240,
+							37, 200, 236, 4, 44, 40, 4, 3, 0, 11, 35, 249, 222, 81, 135, 1, 128, 0,
+							0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						],
+					},
+					Cell {
+						position: Position { row: 0, col: 2 },
+						content: [
+							153, 31, 34, 70, 221, 239, 97, 236, 3, 172, 44, 167, 114, 117, 186,
+							245, 171, 12, 70, 144, 204, 207, 82, 160, 29, 83, 245, 203, 40, 238,
+							96, 131, 68, 96, 9, 136, 151, 88, 218, 72, 79, 55, 193, 228, 71, 193,
+							120, 113, 48, 237, 151, 135, 246, 8, 251, 150, 106, 44, 29, 250, 250,
+							54, 133, 203, 162, 73, 252, 32, 42, 175, 24, 166, 142, 72, 226, 150,
+							163, 206, 115, 0,
+						],
+					},
+					Cell {
+						position: Position { row: 1, col: 1 },
+						content: [
+							146, 211, 61, 65, 166, 68, 252, 65, 196, 167, 211, 64, 223, 151, 33,
+							133, 67, 132, 59, 13, 224, 100, 55, 104, 180, 174, 17, 41, 151, 125,
+							193, 80, 142, 140, 216, 97, 117, 60, 217, 44, 242, 7, 30, 204, 22, 197,
+							12, 179, 88, 163, 102, 4, 54, 208, 14, 161, 193, 25, 34, 179, 35, 234,
+							120, 131, 62, 53, 0, 54, 72, 49, 196, 234, 239, 65, 25, 159, 245, 38,
+							193, 0,
+						],
+					},
+					Cell {
+						position: Position { row: 0, col: 3 },
+						content: [
+							150, 6, 83, 12, 56, 17, 0, 225, 186, 238, 151, 181, 116, 1, 34, 240,
+							174, 192, 98, 201, 60, 208, 50, 215, 90, 231, 2, 27, 17, 204, 140, 30,
+							213, 253, 200, 176, 72, 98, 121, 25, 239, 76, 230, 154, 121, 246, 142,
+							37, 85, 184, 201, 218, 107, 88, 0, 87, 199, 169, 98, 172, 4, 140, 151,
+							65, 162, 162, 190, 205, 20, 95, 67, 114, 73, 59, 170, 52, 243, 140,
+							237, 0,
+						],
+					},
+				];
 
-	// 			let stats = network::FetchStats::new(
-	// 				positions.len(),
-	// 				fetched.len(),
-	// 				Duration::from_secs(0),
-	// 				None,
-	// 			);
-	// 			Box::pin(async move { Ok((fetched, unfetched, stats)) })
-	// 		});
-	// 	mock_client
-	// 		.expect_is_confidence_stored()
-	// 		.with(eq(2))
-	// 		.returning(|_| Ok(true));
-	// 	mock_client
-	// 		.expect_store_confidence()
-	// 		.withf(move |_, block_number| *block_number == 2)
-	// 		.returning(move |_, _| Ok(()));
-	// 	process_block(
-	// 		&mock_client,
-	// 		&mock_network_client,
-	// 		header,
-	// 		header_hash,
-	// 		&cfg,
-	// 		block_tx,
-	// 	)
-	// 	.await
-	// 	.unwrap();
-	// }
+				let stats = network::FetchStats::new(
+					positions.len(),
+					fetched.len(),
+					Duration::from_secs(0),
+					None,
+				);
+				Box::pin(async move { Ok((fetched, unfetched, stats)) })
+			});
+		mock_client
+			.expect_is_confidence_stored()
+			.with(eq(2))
+			.returning(|_| Ok(true));
+		mock_client
+			.expect_store_confidence()
+			.withf(move |_, block_number| *block_number == 2)
+			.returning(move |_, _| Ok(()));
+		process_block(
+			&mock_client,
+			&mock_network_client,
+			header,
+			header_hash,
+			&cfg,
+			block_tx,
+		)
+		.await
+		.unwrap();
+	}
 
-	// #[tokio::test]
-	// pub async fn test_process_blocks_with_rpc() {
-	// 	let (block_tx, _) = broadcast::channel::<types::BlockVerified>(10);
-	// 	let cfg = SyncClientConfig::from(&RuntimeConfig::default());
-	// 	let mut mock_network_client = network::MockClient::new();
-	// 	let db = mem::MemoryDB::default();
-	// 	let mut mock_client = SyncClient::new(db, mock_network_client);
-	// 	let header = default_header();
-	// 	let header_hash: H256 =
-	// 		hex!("3767f8955d6f7306b1e55701b6316fa1163daa8d4cffdb05c3b25db5f5da1723").into();
+	#[tokio::test]
+	pub async fn test_process_blocks_with_rpc() {
+		let (block_tx, _) = broadcast::channel::<types::BlockVerified>(10);
+		let cfg = SyncClientConfig::from(&RuntimeConfig::default());
+		let mut mock_network_client = network::MockClient::new();
+		let mut mock_client = MockClient::new();
+		let header = default_header();
+		let header_hash: H256 =
+			hex!("3767f8955d6f7306b1e55701b6316fa1163daa8d4cffdb05c3b25db5f5da1723").into();
 
-	// 	mock_network_client
-	// 		.expect_fetch_verified()
-	// 		.withf(|&x, _, _, _, _| x == 2)
-	// 		.returning(move |_, _, _, _, positions| {
-	// 			let unfetched = vec![Position { row: 0, col: 3 }];
-	// 			let dht_fetched: Vec<Cell> = vec![
-	// 				Cell {
-	// 					position: Position { row: 0, col: 0 },
-	// 					content: [
-	// 						183, 56, 112, 134, 157, 186, 15, 255, 245, 173, 188, 37, 165, 224, 226,
-	// 						80, 196, 137, 235, 233, 154, 4, 110, 142, 26, 95, 150, 132, 61, 23,
-	// 						202, 212, 101, 6, 235, 6, 102, 188, 206, 147, 36, 121, 128, 63, 240,
-	// 						37, 200, 236, 4, 44, 40, 4, 3, 0, 11, 35, 249, 222, 81, 135, 1, 128, 0,
-	// 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	// 					],
-	// 				},
-	// 				Cell {
-	// 					position: Position { row: 0, col: 2 },
-	// 					content: [
-	// 						153, 31, 34, 70, 221, 239, 97, 236, 3, 172, 44, 167, 114, 117, 186,
-	// 						245, 171, 12, 70, 144, 204, 207, 82, 160, 29, 83, 245, 203, 40, 238,
-	// 						96, 131, 68, 96, 9, 136, 151, 88, 218, 72, 79, 55, 193, 228, 71, 193,
-	// 						120, 113, 48, 237, 151, 135, 246, 8, 251, 150, 106, 44, 29, 250, 250,
-	// 						54, 133, 203, 162, 73, 252, 32, 42, 175, 24, 166, 142, 72, 226, 150,
-	// 						163, 206, 115, 0,
-	// 					],
-	// 				},
-	// 				Cell {
-	// 					position: Position { row: 1, col: 1 },
-	// 					content: [
-	// 						146, 211, 61, 65, 166, 68, 252, 65, 196, 167, 211, 64, 223, 151, 33,
-	// 						133, 67, 132, 59, 13, 224, 100, 55, 104, 180, 174, 17, 41, 151, 125,
-	// 						193, 80, 142, 140, 216, 97, 117, 60, 217, 44, 242, 7, 30, 204, 22, 197,
-	// 						12, 179, 88, 163, 102, 4, 54, 208, 14, 161, 193, 25, 34, 179, 35, 234,
-	// 						120, 131, 62, 53, 0, 54, 72, 49, 196, 234, 239, 65, 25, 159, 245, 38,
-	// 						193, 0,
-	// 					],
-	// 				},
-	// 			];
-	// 			let rpc_fetched: Vec<Cell> = vec![Cell {
-	// 				position: Position { row: 0, col: 3 },
-	// 				content: [
-	// 					150, 6, 83, 12, 56, 17, 0, 225, 186, 238, 151, 181, 116, 1, 34, 240, 174,
-	// 					192, 98, 201, 60, 208, 50, 215, 90, 231, 2, 27, 17, 204, 140, 30, 213, 253,
-	// 					200, 176, 72, 98, 121, 25, 239, 76, 230, 154, 121, 246, 142, 37, 85, 184,
-	// 					201, 218, 107, 88, 0, 87, 199, 169, 98, 172, 4, 140, 151, 65, 162, 162,
-	// 					190, 205, 20, 95, 67, 114, 73, 59, 170, 52, 243, 140, 237, 0,
-	// 				],
-	// 			}];
+		mock_network_client
+			.expect_fetch_verified()
+			.withf(|&x, _, _, _, _| x == 2)
+			.returning(move |_, _, _, _, positions| {
+				let unfetched = vec![Position { row: 0, col: 3 }];
+				let dht_fetched: Vec<Cell> = vec![
+					Cell {
+						position: Position { row: 0, col: 0 },
+						content: [
+							183, 56, 112, 134, 157, 186, 15, 255, 245, 173, 188, 37, 165, 224, 226,
+							80, 196, 137, 235, 233, 154, 4, 110, 142, 26, 95, 150, 132, 61, 23,
+							202, 212, 101, 6, 235, 6, 102, 188, 206, 147, 36, 121, 128, 63, 240,
+							37, 200, 236, 4, 44, 40, 4, 3, 0, 11, 35, 249, 222, 81, 135, 1, 128, 0,
+							0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						],
+					},
+					Cell {
+						position: Position { row: 0, col: 2 },
+						content: [
+							153, 31, 34, 70, 221, 239, 97, 236, 3, 172, 44, 167, 114, 117, 186,
+							245, 171, 12, 70, 144, 204, 207, 82, 160, 29, 83, 245, 203, 40, 238,
+							96, 131, 68, 96, 9, 136, 151, 88, 218, 72, 79, 55, 193, 228, 71, 193,
+							120, 113, 48, 237, 151, 135, 246, 8, 251, 150, 106, 44, 29, 250, 250,
+							54, 133, 203, 162, 73, 252, 32, 42, 175, 24, 166, 142, 72, 226, 150,
+							163, 206, 115, 0,
+						],
+					},
+					Cell {
+						position: Position { row: 1, col: 1 },
+						content: [
+							146, 211, 61, 65, 166, 68, 252, 65, 196, 167, 211, 64, 223, 151, 33,
+							133, 67, 132, 59, 13, 224, 100, 55, 104, 180, 174, 17, 41, 151, 125,
+							193, 80, 142, 140, 216, 97, 117, 60, 217, 44, 242, 7, 30, 204, 22, 197,
+							12, 179, 88, 163, 102, 4, 54, 208, 14, 161, 193, 25, 34, 179, 35, 234,
+							120, 131, 62, 53, 0, 54, 72, 49, 196, 234, 239, 65, 25, 159, 245, 38,
+							193, 0,
+						],
+					},
+				];
+				let rpc_fetched: Vec<Cell> = vec![Cell {
+					position: Position { row: 0, col: 3 },
+					content: [
+						150, 6, 83, 12, 56, 17, 0, 225, 186, 238, 151, 181, 116, 1, 34, 240, 174,
+						192, 98, 201, 60, 208, 50, 215, 90, 231, 2, 27, 17, 204, 140, 30, 213, 253,
+						200, 176, 72, 98, 121, 25, 239, 76, 230, 154, 121, 246, 142, 37, 85, 184,
+						201, 218, 107, 88, 0, 87, 199, 169, 98, 172, 4, 140, 151, 65, 162, 162,
+						190, 205, 20, 95, 67, 114, 73, 59, 170, 52, 243, 140, 237, 0,
+					],
+				}];
 
-	// 			let stats = network::FetchStats::new(
-	// 				positions.len(),
-	// 				dht_fetched.len(),
-	// 				Duration::from_secs(0),
-	// 				Some((rpc_fetched.len(), Duration::from_secs(1))),
-	// 			);
-	// 			let fetched = [&dht_fetched[..], &rpc_fetched[..]].concat();
-	// 			Box::pin(async move { Ok((fetched, unfetched, stats)) })
-	// 		});
+				let stats = network::FetchStats::new(
+					positions.len(),
+					dht_fetched.len(),
+					Duration::from_secs(0),
+					Some((rpc_fetched.len(), Duration::from_secs(1))),
+				);
+				let fetched = [&dht_fetched[..], &rpc_fetched[..]].concat();
+				Box::pin(async move { Ok((fetched, unfetched, stats)) })
+			});
 
-	// 	mock_client
-	// 		.expect_store_confidence()
-	// 		.withf(move |_, block_number| *block_number == 2)
-	// 		.returning(move |_, _| Ok(()));
-	// 	process_block(
-	// 		&mock_client,
-	// 		&mock_network_client,
-	// 		header,
-	// 		header_hash,
-	// 		&cfg,
-	// 		block_tx,
-	// 	)
-	// 	.await
-	// 	.unwrap();
-	// }
+		mock_client
+			.expect_store_confidence()
+			.withf(move |_, block_number| *block_number == 2)
+			.returning(move |_, _| Ok(()));
+		process_block(
+			&mock_client,
+			&mock_network_client,
+			header,
+			header_hash,
+			&cfg,
+			block_tx,
+		)
+		.await
+		.unwrap();
+	}
 }
