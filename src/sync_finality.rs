26cf9d55ff25eacbc960b2665abf58e9bfd10af7
momosh <momcilo@ethernal.tech>
2024-03-07 21:01:04 +0100

    properly interfaced finality client to use new db

--- src/sync_finality.rs
+++ src/sync_finality.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use avail_subxt::primitives::Header;
 use codec::Encode;
 use color_eyre::{
@@ -10,7 +11,11 @@ use sp_core::{
 	ed25519::{self},
 	twox_128, H256,
 };
-use std::sync::{Arc, Mutex};
+use std::{
+	iter::zip,
+	sync::{Arc, Mutex},
+};
+use subxt::{storage::StorageKey, utils::AccountId32};
 use tracing::{error, info, trace};
 
 use crate::{
@@ -23,32 +28,122 @@ use crate::{
 	utils::filter_auth_set_changes,
 };
 
+#[async_trait]
+pub trait Client {
+	fn store_block_header(&self, block_number: u32, header: Header) -> Result<()>;
+	fn get_checkpoint(&self) -> Result<Option<FinalitySyncCheckpoint>>;
+	fn store_checkpoint(&self, checkpoint: FinalitySyncCheckpoint) -> Result<()>;
+	async fn get_paged_storage_keys(
+		&self,
+		key: Vec<u8>,
+		count: u32,
+		start_key: Option<Vec<u8>>,
+		hash: Option<H256>,
+	) -> Result<Vec<StorageKey>>;
+	async fn get_genesis_hash(&self) -> Result<H256>;
+	async fn fetch_set_id_at(&self, block_hash: H256) -> Result<u64>;
+	async fn get_validator_set_at(&self, block_hash: H256) -> Result<Option<Vec<AccountId32>>>;
+	async fn get_session_key_owner_at(
+		&self,
+		block_hash: H256,
+		public_key: ed25519::Public,
+	) -> Result<Option<AccountId32>>;
+	async fn get_block_hash(&self, block_number: u32) -> Result<H256>;
+	async fn get_header_by_hash(&self, block_hash: H256) -> Result<Header>;
+	async fn request_finality_proof(&self, block_number: u32) -> Result<WrappedProof>;
+}
+
 pub struct SyncFinality<T: Database> {
 	db: T,
 	rpc_client: rpc::Client,
 }
 
-impl<T: Database> SyncFinality<T> {
-	fn get_client(&self) -> rpc::Client {
-		self.rpc_client.clone()
+#[async_trait]
+impl<T: Database + Sync> Client for SyncFinality<T> {
+	async fn get_paged_storage_keys(
+		&self,
+		key: Vec<u8>,
+		count: u32,
+		start_key: Option<Vec<u8>>,
+		hash: Option<H256>,
+	) -> Result<Vec<StorageKey>> {
+		self.rpc_client
+			.get_paged_storage_keys(key, count, start_key, hash)
+			.await
+			.wrap_err("Finality Sync Client failed to get paged Storage Keys")
+	}
+
+	async fn get_genesis_hash(&self) -> Result<H256> {
+		self.rpc_client
+			.get_genesis_hash()
+			.await
+			.wrap_err("Finality Sync Client failed to get Genesis Hash")
+	}
+
+	async fn fetch_set_id_at(&self, block_hash: H256) -> Result<u64> {
+		self.rpc_client
+			.fetch_set_id_at(block_hash)
+			.await
+			.wrap_err("Finality Sync Client failed to fetch Set ID at provided Block Hash")
+	}
+
+	async fn get_validator_set_at(&self, block_hash: H256) -> Result<Option<Vec<AccountId32>>> {
+		self.rpc_client
+			.get_validator_set_at(block_hash)
+			.await
+			.wrap_err("Finality Sync Client failed to get Validator Set at provided Block Hash")
+	}
+
+	async fn get_session_key_owner_at(
+		&self,
+		block_hash: H256,
+		public_key: ed25519::Public,
+	) -> Result<Option<AccountId32>> {
+		self.rpc_client
+			.get_session_key_owner_at(block_hash, public_key)
+			.await
+			.wrap_err(
+				"Finality Sync Client failed to get Session Key Owner for provided Block Hash",
+			)
+	}
+
+	async fn get_block_hash(&self, block_number: u32) -> Result<H256> {
+		self.rpc_client
+			.get_block_hash(block_number)
+			.await
+			.wrap_err("Finality Sync Client failed to get Block Hash")
+	}
+
+	async fn get_header_by_hash(&self, block_hash: H256) -> Result<Header> {
+		self.rpc_client
+			.get_header_by_hash(block_hash)
+			.await
+			.wrap_err("Finality Sync Client failed to get Header by Hash")
+	}
+
+	async fn request_finality_proof(&self, block_number: u32) -> Result<WrappedProof> {
+		self.rpc_client
+			.request_finality_proof(block_number)
+			.await
+			.wrap_err("Finality Sync Client failed to request Finality Proof")
 	}
 
 	fn store_block_header(&self, block_number: u32, header: Header) -> Result<()> {
 		self.db
 			.put(Key::BlockHeader(block_number), header)
-			.wrap_err("Finality Sync failed to store Block Header")
+			.wrap_err("Finality Sync Client failed to store Block Header")
 	}
 
 	fn get_checkpoint(&self) -> Result<Option<FinalitySyncCheckpoint>> {
 		self.db
 			.get(Key::FinalitySyncCheckpoint)
-			.wrap_err("Finality Sync failed to get Checkpoint")
+			.wrap_err("Finality Sync Client failed to get Checkpoint")
 	}
 
 	fn store_checkpoint(&self, checkpoint: FinalitySyncCheckpoint) -> Result<()> {
 		self.db
 			.put(Key::FinalitySyncCheckpoint, checkpoint)
-			.wrap_err("Finality Sync failed to store Checkpoint")
+			.wrap_err("Finality Sync Client failed to store Checkpoint")
 	}
 }
 
@@ -60,21 +155,21 @@ const GRANDPA_KEY_ID: [u8; 4] = *b"gran";
 const GRANDPA_KEY_LEN: usize = 32;
 
 async fn get_valset_at_genesis(
-	rpc_client: rpc::Client,
+	client: &impl Client,
 	genesis_hash: H256,
 ) -> Result<Vec<ed25519::Public>> {
 	let mut k1 = twox_128("Session".as_bytes()).to_vec();
 	let mut k2 = twox_128("KeyOwner".as_bytes()).to_vec();
 	k1.append(&mut k2);
 
-	let validator_set_pre = rpc_client
+	let validator_set_pre = client
 		.get_validator_set_at(genesis_hash)
 		.await
 		.wrap_err("Couldn't get initial validator set")?
 		.ok_or_else(|| eyre!("Validator set is empty!"))?;
 
 	// Get all grandpa session keys from genesis (GRANDPA ed25519 keys)
-	let grandpa_keys_and_account = rpc_client
+	let grandpa_keys = client
 		// get all storage keys that correspond to Session_KeyOwner query, then filter by "gran"
 		// perhaps there is a better way, but I don't know it
 		.get_paged_storage_keys(k1.clone(), 1000, None, Some(genesis_hash))
@@ -91,19 +186,21 @@ async fn get_valset_at_genesis(
 		})
 		.map(|e| e.as_slice()[e.len() - GRANDPA_KEY_LEN..].to_vec())
 		.map(|e| ed25519::Public::from_raw(e.try_into().expect("Vector isn't 32 bytes long")))
-		.map(|e: ed25519::Public| {
-			let c = rpc_client.clone();
-			async move {
-				let k = c
-					.get_session_key_owner_at(genesis_hash, e)
-					.await
-					.expect("Couldn't get session key owner for grandpa key!")
-					.expect("Result is empty (grandpa key has no owner?)");
-				(e, k)
-			}
-		});
+		.collect::<Vec<ed25519::Public>>();
 
-	let grandpa_keys_and_account = join_all(grandpa_keys_and_account).await;
+	let grandpa_account_results = join_all(
+		grandpa_keys
+			.clone()
+			.iter()
+			.map(|&e| client.get_session_key_owner_at(genesis_hash, e)),
+	)
+	.await;
+	let grandpa_accounts = grandpa_account_results.into_iter().map(|a| {
+		a.expect("Couldn't get session key owner for grandpa key!")
+			.expect("Result is empty (grandpa key has no owner?)")
+	});
+
+	let grandpa_keys_and_account = zip(grandpa_keys, grandpa_accounts);
 
 	// Filter just the keys that are found in the initial validator set
 	let validator_set = grandpa_keys_and_account
@@ -114,27 +211,26 @@ async fn get_valset_at_genesis(
 	Ok(validator_set)
 }
 
-pub async fn run<T: Database>(
-	sync_finality: SyncFinality<T>,
+pub async fn run(
+	client: impl Client,
 	shutdown: Controller<String>,
 	state: Arc<Mutex<State>>,
 	from_header: Header,
 ) {
-	if let Err(error) = sync(sync_finality, state, from_header).await {
+	if let Err(error) = sync(client, state, from_header).await {
 		error!("Cannot sync finality {error}");
 		let _ = shutdown.trigger_shutdown(format!("Cannot sync finality {error:#}"));
 	};
 }
 
-pub async fn sync<T: Database>(
-	sync_finality: SyncFinality<T>,
+pub async fn sync(
+	client: impl Client,
 	state: Arc<Mutex<State>>,
 	mut from_header: Header,
 ) -> Result<()> {
-	let rpc_client = sync_finality.get_client();
-	let gen_hash = rpc_client.get_genesis_hash().await?;
+	let gen_hash = client.get_genesis_hash().await?;
 
-	let checkpoint = sync_finality.get_checkpoint()?;
+	let checkpoint = client.get_checkpoint()?;
 
 	info!("Starting finality validation sync.");
 	let mut set_id: u64;
@@ -147,9 +243,9 @@ pub async fn sync<T: Database>(
 		curr_block_num = ch.number;
 	} else {
 		info!("No checkpoint found, starting from genesis.");
-		validator_set = get_valset_at_genesis(rpc_client.clone(), gen_hash).await?;
+		validator_set = get_valset_at_genesis(&client, gen_hash).await?;
 		// get set_id from genesis. Should be 0.
-		set_id = rpc_client
+		set_id = client
 			.fetch_set_id_at(gen_hash)
 			.await
 			.wrap_err(format!("Couldn't get set_id at {}", gen_hash))?;
@@ -160,7 +256,7 @@ pub async fn sync<T: Database>(
 
 	info!("Syncing finality from {curr_block_num} up to block no. {last_block_num}");
 
-	let mut prev_hash = rpc_client
+	let mut prev_hash = client
 		.get_block_hash(curr_block_num - 1)
 		.await
 		.wrap_err("Hash doesn't exist?")?;
@@ -169,18 +265,18 @@ pub async fn sync<T: Database>(
 			info!("Finished verifying finality up to block no. {last_block_num}!");
 			break;
 		}
-		let hash = rpc_client
+		let hash = client
 			.get_block_hash(curr_block_num)
 			.await
 			.wrap_err(format!(
 				"Couldn't get hash for block no. {}",
 				curr_block_num
 			))?;
-		from_header = rpc_client
+		from_header = client
 			.get_header_by_hash(hash)
 			.await
 			.wrap_err(format!("Couldn't get header for {}", hash))?;
-		sync_finality.store_block_header(curr_block_num, from_header.clone())?;
+		client.store_block_header(curr_block_num, from_header.clone())?;
 
 		assert_eq!(
 			from_header.parent_hash, prev_hash,
@@ -194,7 +290,7 @@ pub async fn sync<T: Database>(
 			continue;
 		}
 
-		let proof: WrappedProof = rpc_client
+		let proof: WrappedProof = client
 			.request_finality_proof(curr_block_num)
 			.await
 			.wrap_err(format!(
@@ -202,7 +298,7 @@ pub async fn sync<T: Database>(
 				curr_block_num
 			))?;
 		let proof_block_hash = proof.0.block;
-		let p_h = rpc_client
+		let p_h = client
 			.get_header_by_hash(proof_block_hash)
 			.await
 			.wrap_err(format!("Couldn't get header for {}", proof_block_hash))?;
@@ -221,7 +317,7 @@ pub async fn sync<T: Database>(
 			.map(|a| ed25519::Public::from_raw(a.0 .0 .0 .0))
 			.collect();
 		set_id += 1;
-		sync_finality.store_checkpoint(FinalitySyncCheckpoint {
+		client.store_checkpoint(FinalitySyncCheckpoint {
 			number: curr_block_num,
 			set_id,
 			validator_set: validator_set.clone(),
