c0504ec6d9f2d3eadf0602b952eda1f3cb72d8fa
momosh <momcilo@ethernal.tech>
2024-07-15 16:01:09 +0200

    fmt fixes

--- bootstrap/src/telemetry/mod.rs
+++ bootstrap/src/telemetry/mod.rs
@@ -4,12 +4,12 @@ use async_trait::async_trait;
 pub mod otlp;
 
 pub enum MetricValue {
-    KadRoutingPeerNum(usize),
-    HealthCheck(),
+	KadRoutingPeerNum(usize),
+	HealthCheck(),
 }
 
 #[async_trait]
 pub trait Metrics {
-    async fn record(&self, value: MetricValue) -> Result<()>;
-    async fn set_multiaddress(&self, multiaddrs: String);
+	async fn record(&self, value: MetricValue) -> Result<()>;
+	async fn set_multiaddress(&self, multiaddrs: String);
 }
--- relay/src/telemetry/mod.rs
+++ relay/src/telemetry/mod.rs
@@ -4,13 +4,13 @@ use anyhow::Result;
 use async_trait::async_trait;
 
 pub enum MetricValue {
-    HealthCheck(),
+	HealthCheck(),
 }
 
 #[async_trait]
 pub trait Metrics {
-    async fn record(&self, value: MetricValue) -> Result<()>;
-    async fn get_multiaddress(&self) -> String;
-    async fn set_multiaddress(&self, multiaddrs: String);
-    async fn set_ip(&self, ip: String);
+	async fn record(&self, value: MetricValue) -> Result<()>;
+	async fn get_multiaddress(&self) -> String;
+	async fn set_multiaddress(&self, multiaddrs: String);
+	async fn set_ip(&self, ip: String);
 }
