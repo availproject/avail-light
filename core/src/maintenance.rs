8eafcda5c97e2380d68781403567ee7e6aecef58
sh3ll3x3c <ljubisa@ethernal.tech>
2024-07-24 22:42:08 +0200

    fix `operating_mode` metric attribute not switching properly

--- core/src/maintenance.rs
+++ core/src/maintenance.rs
@@ -51,13 +51,14 @@ pub async fn process_block(
 
 	// Reconfigure Kademlia mode if needed
 	if maintenance_config.automatic_server_mode {
-		p2p_client
+		let new_mode = p2p_client
 			.reconfigure_kademlia_mode(
 				maintenance_config.total_memory_gb_threshold,
 				maintenance_config.num_cpus_threshold,
 			)
 			.await
 			.wrap_err("Unable to reconfigure kademlia mode")?;
+		metrics.update_operating_mode(new_mode).await;
 	}
 
 	let peers_num_metric = MetricValue::DHTConnectedPeers(peers_num);
