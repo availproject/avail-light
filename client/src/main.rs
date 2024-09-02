c9b31e5b79f11c4922874aa869047caab068e34e
momosh <93727411+momosh-ethernal@users.noreply.github.com>
2024-09-02 10:52:54 +0200

    Outputting events from Light Client module

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,6 +5,7 @@ use avail_light_core::{
 	api,
 	consts::EXPECTED_SYSTEM_VERSION,
 	data::{self, ClientIdKey, Database, IsFinalitySyncedKey, IsSyncedKey, LatestHeaderKey, DB},
+	light_client, maintenance,
 	network::{
 		self,
 		p2p::{self, BOOTSTRAP_LIST_EMPTY_MESSAGE},
@@ -31,7 +32,7 @@ use color_eyre::{
 	Result,
 };
 use std::{fs, path::Path, sync::Arc};
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, mpsc};
 use tracing::{error, info, span, trace, warn, Level};
 
 #[cfg(feature = "network-analysis")]
@@ -288,7 +289,7 @@ async fn run(
 	}
 
 	let static_config_params: MaintenanceConfig = (&cfg).into();
-	spawn_in_span(shutdown.with_cancel(avail_light_core::maintenance::run(
+	spawn_in_span(shutdown.with_cancel(maintenance::run(
 		p2p_client.clone(),
 		ot_metrics.clone(),
 		block_rx,
@@ -302,14 +303,15 @@ async fn run(
 	};
 
 	let light_network_client = network::new(p2p_client, rpc_client, pp, cfg.disable_rpc);
-
-	spawn_in_span(shutdown.with_cancel(avail_light_core::light_client::run(
+	let (lc_sender, _) = mpsc::channel::<light_client::OutputEvent>(1000);
+	spawn_in_span(shutdown.with_cancel(light_client::run(
 		db.clone(),
 		light_network_client,
 		(&cfg).into(),
 		ot_metrics.clone(),
 		channels,
 		shutdown.clone(),
+		lc_sender,
 	)));
 
 	ot_metrics.count(MetricCounter::Starts).await;
