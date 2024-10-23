5053b2684dbb7c7965eecddc7c38997e4cef78b9
Aleksandar Terentić <aleksandar@ethernal.tech>
2024-10-23 11:14:42 +0200

    Add back origin attribute to all client metrics

--- client/CHANGELOG.md
+++ client/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.12.2
+
+- Add back origin attribute to metrics
+
 ## [1.12.1](https://github.com/availproject/avail-light/releases/tag/avail-light-client-v1.12.1) - 2024-10-22
 
 - Update avail-light-core to 1.0.3
--- fat/src/main.rs
+++ fat/src/main.rs
@@ -199,6 +199,7 @@ async fn run(config: Config, db: DB, shutdown: Controller<String>) -> Result<()>
 
 	let metric_attributes = vec![
 		("role".to_string(), "fat".to_string()),
+		("origin".to_string(), Origin::FatClient.to_string()),
 		("version".to_string(), version.to_string()),
 		("peerID".to_string(), p2p_peer_id.to_string()),
 		("partition_size".to_string(), partition_size),
