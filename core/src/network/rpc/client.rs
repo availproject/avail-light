601eb847024c3c913e06d9f2bdc6c1fc6cb54d6d
vbhattaccmu <53374818+vbhattaccmu@users.noreply.github.com>
2024-11-12 20:14:05 +0700

    fmt

--- core/src/network/rpc/client.rs
+++ core/src/network/rpc/client.rs
@@ -23,7 +23,6 @@ use tracing::{error, info, warn};
 
 use super::{configuration::RetryConfig, Node, Nodes, Subscription, WrappedProof};
 use crate::{
-	api::types::Base64,
 	data::{Database, RpcNodeKey, SignerNonceKey},
 	network::rpc::OutputEvent as RpcEvent,
 	shutdown::Controller,
