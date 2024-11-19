e331baf2d83ad3c8a7ad82c63d272ce9a152650d
sh3ll3x3c <ljubisa@ethernal.tech>
2024-11-19 17:40:01 +0100

    implement initial monitor client scaffold

---
+++ monitor-tests/src/main.rs
@@ -0,0 +1,48 @@
+use avail_light_core::{
+	data,
+	network::p2p::{self, configuration::LibP2PConfig},
+	shutdown::Controller,
+	types::ProjectName,
+};
+use clap::Parser;
+use color_eyre::{eyre::Context, Result};
+use tracing::info;
+
+mod config;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	info!("Starting Avail monitors...");
+	let version = clap::crate_version!();
+
+	let opts = config::CliOpts::parse();
+
+	let db = data::DB::open(&opts.db_path)?;
+
+	let config = config::load(&opts)?;
+	let shutdown = Controller::new();
+
+	let (p2p_keypair, p2p_peer_id) = p2p::identity(&config.libp2p, db.clone())?;
+
+	let (p2p_client, p2p_event_loop, p2p_event_receiver) = p2p::init(
+		config.libp2p.clone(),
+		ProjectName::new("avail".to_string()),
+		p2p_keypair,
+		version,
+		opts.network.genesis_hash(),
+		true,
+		shutdown.clone(),
+		db.clone(),
+	)
+	.await?;
+
+	let addrs = vec![config.libp2p.tcp_multiaddress()];
+
+	p2p_client
+		.start_listening(addrs)
+		.await
+		.wrap_err("Listening on TCP not to fail.")?;
+	info!("TCP listener started on port {}", config.libp2p.port);
+
+	Ok(())
+}
