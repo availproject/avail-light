40cf043b52ee60e599969cb51881baf5166ad579
Jeremy <jeremias@availproject.org>
2025-01-18 08:43:40 -0300

    lint

--- core/src/telemetry/otlp.rs
+++ core/src/telemetry/otlp.rs
@@ -231,7 +231,7 @@ pub fn initialize(
 		.with_endpoint(&ot_config.ot_collector_endpoint)
 		.with_protocol(Protocol::Grpc)
 		.with_timeout(Duration::from_secs(ot_config.ot_export_timeout))
-        .with_temporality(Temporality::Delta)
+		.with_temporality(Temporality::Delta)
 		.build()?;
 
 	let reader = PeriodicReader::builder(exporter, Tokio)
