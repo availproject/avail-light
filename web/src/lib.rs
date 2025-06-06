7ba452d8ac0fe2f274c617f8324bc204c180ac9b
vbhattaccmu <53374818+vbhattaccmu@users.noreply.github.com>
2025-06-06 10:21:13 +0400

    update confidence logging

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -341,11 +341,11 @@ pub async fn latest_block(network_param: Option<String>) -> String {
 
 	let network_client = network::new_rpc(rpc_client, pp);
 
-	let confidence =
+	let block_verified =
 		light_client::process_block(db, &network_client, 99.9, header, received_at, lc_sender)
 			.await
 			.unwrap()
-			.map(|confidence| format!("{confidence}"))
+			.map(|block_verified| format!("{:?}", block_verified.confidence))
 			.unwrap_or("none".to_string());
 
 	format!("Confidence for block {} is {confidence}.", block.number())
