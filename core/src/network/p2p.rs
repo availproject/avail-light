8549e830a74841fb9e245b2ad2a088ed48207574
Aleksandar Terentić <aleksandar@ethernal.tech>
2025-09-02 14:30:03 +0200

    Increment minimum P2P supported versions

--- core/src/network/p2p.rs
+++ core/src/network/p2p.rs
@@ -72,9 +72,9 @@ use libp2p_allow_block_list as allow_block_list;
 #[cfg(not(target_arch = "wasm32"))]
 pub use restart::{forward_p2p_events, init_and_start_p2p_client, p2p_restart_manager};
 
-const MINIMUM_SUPPORTED_BOOTSTRAP_VERSION: &str = "0.5.0";
-const MINIMUM_SUPPORTED_FAT_CLIENT_VERSION: &str = "1.9.2";
-const MINIMUM_SUPPORTED_LIGHT_CLIENT_VERSION: &str = "1.9.2";
+const MINIMUM_SUPPORTED_BOOTSTRAP_VERSION: &str = "0.5.3";
+const MINIMUM_SUPPORTED_FAT_CLIENT_VERSION: &str = "1.12.8";
+const MINIMUM_SUPPORTED_LIGHT_CLIENT_VERSION: &str = "1.13.0";
 pub const MINIMUM_P2P_CLIENT_RESTART_INTERVAL: u64 = 60; // seconds
 
 pub const BOOTSTRAP_LIST_EMPTY_MESSAGE: &str = r#"
